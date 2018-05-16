bc9167cc30f167faa341ddcafba89ddb13a75883
Johannes <johannesmannhardt@web.de>
2018-05-16 14:32:56 +0200

    Git Commit

--- Aufgabe02_5-9/Aufgabe02_5-9.ino
+++ Aufgabe02_5-9/Aufgabe02_5-9.ino
@@ -1,14 +1,33 @@
 #include <SoftwareSerial.h>
 
-// Copyright 2015: Jan Burchard
-// HWP Code: Low level ADC
+#define tonePin 12
+boolean led = true;
 
-void setup() {
-
+void setup()
+{
+  pinMode(tonePin, OUTPUT);  // Ausgabe LED festlegen
+  Serial.begin(9600);
+  // Timer 1
+  noInterrupts();           // Alle Interrupts temporär abschalten
+  TCCR2A = 0;
+  TCCR2B = 0;
+  TCNT2 = 0;
+  OCR2A = 154;
+  TCCR2B |= (1 << CS21);
+  TCCR2B |= (1 << CS22);
+  TIMSK2 |= (1 << OCIE1A);  
+  interrupts();             
 }
 
-
-void loop() {
+ISR(TIMER2_COMPA_vect)        
+{
+  TCNT2 = 0;    
+  led = not led;
+  digitalWrite(tonePin, led);
+  // LED ein und aus
 }
 
-
+void loop()
+{
+  // Wir könnten hier zusätzlichen Code integrieren
+}
