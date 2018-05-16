eb557b298a89b518b834a37fffb3985f492c18be
Johannes <johannesmannhardt@web.de>
2018-05-16 15:51:05 +0200

    Git Commit

--- Aufgabe02_5-9/Aufgabe02_5-9.ino
+++ Aufgabe02_5-9/Aufgabe02_5-9.ino
@@ -3,10 +3,13 @@
 #define tonePin 12
 boolean led = true;
 int prescalers[7] = {1, 8, 32, 64, 128, 256, 1024};
+volatile uint32_t tCount = 0;
+unsigned long f = 1000;
 
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(tonePin, OUTPUT);
+  every1ms();
 }
 
 
@@ -37,33 +40,27 @@ void toneAdjustableFreq(int freq) {
       OCR2A = result;
       switch (prescalers[i]) {
         case (1):
-        Serial.println("yooffo");
+          TCCR2B |= (1 << CS20);
           break;
         case (8):
-        Serial.println("yooasdfo");
           TCCR2B |= (1 << CS21);
           break; 
         case (32):
-        Serial.println("yoooahgrae");
           TCCR2B |= (1 << CS20);
           TCCR2B |= (1 << CS21);
           break; 
         case (64):
-       Serial.println("yoooaregr");
           TCCR2B |= (1 << CS22);
           break; 
         case (128):
-        Serial.println("yooargeg");
           TCCR2B |= (1 << CS20);
           TCCR2B |= (1 << CS22);
           break; 
         case (256):
-       Serial.println("yoooa43r");
           TCCR2B |= (1 << CS21);
           TCCR2B |= (1 << CS22);
           break; 
         case (1024):
-          Serial.println("yooo3");
           TCCR2B |= (1 << CS20);
           TCCR2B |= (1 << CS21);
           TCCR2B |= (1 << CS22);
@@ -76,15 +73,32 @@ void toneAdjustableFreq(int freq) {
   }
 
 
+// Excercise 7
+void every1ms() {
+  noInterrupts();
+  TCCR2A = 0;
+  TCCR2B = 0;
+  TCNT2 = 0;
+  OCR2A = 30;
+  TCCR2B |= (1 << CS21);
+  TCCR2B |= (1 << CS22);
+  TIMSK2 |= (1 << OCIE1A);  
+  interrupts();  
+  }
+
+
 // Interrupt
 ISR(TIMER2_COMPA_vect) {
+  tCount += 1;
   TCNT2 = 0;  
   led = not led;
   digitalWrite(tonePin, led);
   }
 
 
-void loop()
-{
-  
+void loop() {
+  if (millis() > f) {
+    f += 1000;
+    Serial.println(tCount);
+  }
 }
